6b5878a02efdbe2df5d947dc61d871bfbd9b481a
theroar <1377202477@qq.com>
2018-03-23 15:52:08 +0800

    提交修改

--- src/main/java/com/example/MainTest.java
+++ src/main/java/com/example/MainTest.java
@@ -4,17 +4,25 @@ import com.example.core.config.IConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
 public class MainTest {
 
     /**
      * 进行rebase测试
      * @param args
      */
-    public static void main(String args[]) {
-        Object o = IConfig.class.getResourceAsStream("/spring/config.properties");
+    public static void main(String args[]) throws UnsupportedEncodingException {
+        /**Object o = IConfig.class.getResourceAsStream("/spring/config.properties");
         if (o == null) {
             System.out.print("失败");
-        }
+        }**/
+        String strUrlEn = URLEncoder.encode("http://www.baidu.com", "utf-8");
+        System.out.println(strUrlEn);
+        String strUrlDe = URLDecoder.decode(strUrlEn, "utf-8");
+        System.out.println(strUrlDe);
 
     }
 }
